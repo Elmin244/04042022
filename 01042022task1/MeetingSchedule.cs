c68c59e59d46ee3db0d144d819de6e648659dbc1
unknown <tu7d5w0m5@code.edu.az>
2022-04-04 00:38:26 -0800

    first commit

---
+++ 01042022task1/MeetingSchedule.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01042022task1
+{
+    class MeetingSchedule
+    {
+        private List<Meeting> _meetings;
+        
+        public List<Meeting> Meetings
+        {
+            get => _meetings;
+        }
+        public MeetingSchedule()
+        {
+            _meetings = new List<Meeting>();
+        }
+        public void SetMeeting(string name,DateTime fromDate,DateTime toDate)
+        {
+            if (fromDate < toDate)
+            {
+                Meeting meeting = new Meeting();
+                meeting.Name = name;
+                meeting.FromDate = fromDate;
+                meeting.ToDate = toDate;
+                if (_meetings.Count == 0)
+                {
+                    _meetings.Add(meeting);
+                }
+                else
+                {
+                    bool HasTrue = false;
+                    foreach (var meet in _meetings)
+                    {
+                        if (fromDate > meet.ToDate || toDate < meet.ToDate)
+                        {
+                            HasTrue = true;
+                        }
+                        else
+                        {
+                            throw new Exception("Bu vaxtda meeting var:");
+                        }
+                    }
+                    if (HasTrue)
+                    {
+                        _meetings.Add(meeting);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("fromDate daxil edin evvela,sonra toDate");
+            }
+            public int FindMeetingCount(DateTime time)
+            {
+                if (time==null)
+                {
+                    throw new Exception("Time null ola bilmez");
+                }
+                else
+                {
+                    int count = 0;
+                    foreach (Meeting meet in _meetings)
+                    {
+                        if (time<meet.FromDate)
+                        {
+                            count++;
+                        }
+                    }
+                    return count;
+                }
+
+            }
+            public bool IsExistsMeetingByName(string name)
+            {
+                if (string.IsNullOrWhiteSpace(name)
+                {
+                    throw new Exception("Null daxil etmek olmaz");
+                }
+                else
+                {
+                    foreach  (Meeting meet in _meetings)
+                    {
+                        if (meet.Name.Contains(name)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ 01042022task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01042022task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeetingSchedule meetingSchedule = new MeetingSchedule();
+            try
+            {
+                meetingSchedule.SetMeeting("Rektorla gorus ", new DateTime(2022, 04, 1, 13, 00, 00), new DateTime(2022, 04, 01, 15, 10, 00));
+                    meetingSchedule.SetMeeting("Dekanla gorus ", new DateTime(2022, 04, 1, 13, 00, 00), new DateTime(2022, 04, 01, 15, 10, 00));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            foreach (Meeting meet in meetingSchedule.Meetings)
+            {
+                Console.WriteLine(meet.Name);
+            }
+        }
+    }
+}
